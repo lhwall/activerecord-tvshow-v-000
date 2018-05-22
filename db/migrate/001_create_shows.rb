27bd8862986aa487556d399ae980eecb308657ee
Lucy Wall <lhwall@gmail.com>
2018-05-22 12:30:48 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,7 @@
 class CreateShows < ActiveRecord::Migration[4.2]
 
   def change
-    create_table :costumes do |t|
+    create_table :shows do |t|
       t.text :name
       t.text :network
       t.text :day
