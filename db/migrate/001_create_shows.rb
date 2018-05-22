d35871b5ce98d4638de2b95827505969fb7713a5
Lucy Wall <lhwall@gmail.com>
2018-05-22 12:29:51 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -6,7 +6,6 @@ class CreateShows < ActiveRecord::Migration[4.2]
       t.text :network
       t.text :day
       t.integer :rating
-      t.timestamps
     end
   end
 
--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -4,4 +4,4 @@ class CreateShows < ActiveRecord::Migration[4.2]
     add_column :shows, :season, :string
   end
 
-end 
+end
